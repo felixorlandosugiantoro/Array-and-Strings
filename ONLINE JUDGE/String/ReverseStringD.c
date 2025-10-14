a0a24b164a9f152de006f0a018eca636171a54bf
felixorlandosugiantoro <felixorlandosugiantoroapple@gmail.com>
2025-10-14 19:55:44 +0700

    ala

--- ONLINE JUDGE/String/ReverseStringD.c
+++ ONLINE JUDGE/String/ReverseStringD.c
@@ -1,33 +1,21 @@
+
 #include <stdio.h>
 #include <string.h>
-
 int main() {
     int a;
-    // Buffer kata dibuat 1001 untuk menampung string dengan panjang maksimal 1000 + null terminator.
-    char kata[1001]; 
+    char kata[1000];
     
-    scanf("%d", &a);
+    scanf ("%d", &a);
 
-    for (int i = 1; i <= a; i++) {
-        scanf("%s", kata);
-        
-        // Hitung panjang string sekali saja di awal untuk efisiensi.
+    for (int i = 1; i <= a ; i++){
+        scanf ("%s", kata);
         int length = strlen(kata);
-        
-        // Loop untuk menukar posisi karakter dari tepi ke tengah.
-        for (int j = 0; j < length / 2; j++) {
-            // Simpan karakter awal ke variabel sementara.
-            char temp = kata[j];
-            
-            // Ganti karakter awal dengan karakter akhir.
-            // Gunakan variabel 'length' yang sudah dihitung, bukan memanggil strlen() berulang kali.
-            kata[j] = kata[length - j - 1];
-            
-            // Ganti karakter akhir dengan karakter yang tadi disimpan di temp.
-            kata[length - j - 1] = temp;
+        for (int j = 0; j < length/2 ; j++){
+        char temp = kata[j];
+        kata[j] = kata[strlen(kata) - j - 1];
+        kata[strlen(kata) - j - 1] = temp;
         }
-        
-        printf("Case #%d: %s\n", i, kata);
+        printf ("Case #%d : %s\n", i, kata);
     }
 
     return 0;
