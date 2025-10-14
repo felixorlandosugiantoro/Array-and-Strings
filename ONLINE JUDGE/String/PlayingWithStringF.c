6a9ef350e9aa4b304f326a55200108f6e720d070
felixorlandosugiantoro <felixorlandosugiantoroapple@gmail.com>
2025-10-14 19:10:13 +0700

    new

--- ONLINE JUDGE/String/PlayingWithStringF.c
+++ ONLINE JUDGE/String/PlayingWithStringF.c
@@ -2,29 +2,41 @@
 #include <string.h>
 
 int main() {
+    // Siapin variabel buat nampung berapa banyak baris kata yang mau diinput
     int jumlah_kata;
+    
+    // Minta user masukin jumlah barisnya
     scanf("%d", &jumlah_kata);
 
+    // Ini penting buat 'nangkep' karakter enter setelah input angka, biar nggak ngaco di `fgets` nanti
     getchar(); 
 
+    // Ulangin prosesnya sebanyak jumlah kata yang tadi diinput
     for (int i = 1; i <= jumlah_kata; i++) {
+        // Bikin tempat buat nampung katanya, ukurannya 1001 buat jaga-jaga
         char kata[1001];
 
-        // Utk baca seluruh baris, termasuk spasi, sampai 1000 karakter
+        // Baca satu baris penuh dari input user, termasuk spasi
         fgets(kata, sizeof(kata), stdin);
 
+        // `fgets` itu suka bawa karakter 'enter' di akhir. Baris ini buat ngilangin 'enter' itu.
         kata[strcspn(kata, "\n")] = '\0';
         
+        // Hitung panjang kata yang udah bersih dari 'enter'
         int panjang = strlen(kata);
 
+        // Cetak awalan buat outputnya, contoh: "Case #1: "
         printf("Case #%d: ", i);
 
+        // Sekarang kita putar balik katanya, mulai dari huruf paling belakang
         for (int j = panjang - 1; j >= 0; j--) {
+            // Cetak hurufnya satu per satu dari belakang
             printf("%c", kata[j]);
         }
         
+        // Kasih baris baru biar rapi setelah selesai satu kata
         printf("\n");
     }
 
-    return 0;
+    return 0; // Tandanya program selesai dengan baik
 }
--- ONLINE JUDGE/String/ReverseStringD.c
+++ ONLINE JUDGE/String/ReverseStringD.c
@@ -2,29 +2,27 @@
 #include <string.h>
 
 int main() {
-    // Oke, pertama kita baca dulu ada berapa kata yang mau dibalik
     int jumlah_kata;
     scanf("%d", &jumlah_kata);
 
-    // Kalau sudah, ini buat perulangan untuk setiap katanya
-    for (int i = 1; i <= jumlah_kata; i++) {
-        // Siapkan tempat untuk menampung katanya
-        char kata[1001];
-        scanf("%s", kata);
+    getchar(); 
 
-        // Hitung panjang katanya dulu pakai strlen
+    for (int i = 1; i <= jumlah_kata; i++) {
+        char kata[1001];
+
+        // Utk baca seluruh baris, termasuk spasi, sampai 1000 karakter
+        fgets(kata, sizeof(kata), stdin);
+
+        kata[strcspn(kata, "\n")] = '\0';
+        
         int panjang = strlen(kata);
 
-        // Cetak awalan "Case #..." sesuai contoh di soal
         printf("Case #%d: ", i);
 
-        // Nah, ini bagian intinya. Kita cetak hurufnya satu-satu
-        // tapi dari belakang (dari indeks terakhir sampai ke 0)
         for (int j = panjang - 1; j >= 0; j--) {
             printf("%c", kata[j]);
         }
         
-        // Terakhir, kasih baris baru biar rapi
         printf("\n");
     }
 
